094b75f2f0864584f4b46c1e698df0985e41e69f
Joseph Peters <joseph.peters75.com>
2023-11-08 19:32:00 -0500

    update

---
+++ Unit 103/201ops3student.sh
@@ -0,0 +1,16 @@
+!/bin/bash
+# Script: Ops 201 Class 03 Ops Challenge Solution
+# Author: Joseph Peters
+# Purpose: Conditonal Statements
+
+echo "Pick a number between 01 and 09 (0-9)"
+
+read number
+
+if [[ $number = 05 ]]; then
+        echo "$number is equal to 5"
+elif [[ $number > 05 ]]; then
+        echo "$number is greater than 5"
+elif [[ $number < 05 ]]; then
+        echo "$number is less than 5"
+fi 
