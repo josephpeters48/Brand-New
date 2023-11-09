2a183db27bbb7eed19dc4642985ee411aa30db12
Joseph Peters <joseph.peters75.com>
2023-11-08 22:01:37 -0500

    update

---
+++ Unit 103/201ops4students.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+# Lets create a function in bash that adds two number together
+# Stretch goal can you do subtraction, multipliaction or division
+# You will need to declare two variables
