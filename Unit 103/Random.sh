a82ec034a7f004d4a109e72a1eb3441763757081
Joseph Peters <joseph.peters75.com>
2023-11-10 16:45:52 -0500

    update

---
+++ Unit 103/Random.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "What is your name?"
+
+read name
+
+guess=$(( $RANDOM % 2 ))
+echo "Pick a random number from 0-1 $name? (0/1)"
+
+read math
+
+if [[ $guess == $math ]]; then
+        echo "Great Job! $name"
+else
+        echo "Better Luck Next Time $name"
+exit 1
+fi
+add()
+{
+c=`expr $a + $b`
+c=`expr $a + $b`
+d=`expr $a - $b`
+e=`expr $a \* $b`
+f=`expr $a / $b`
+echo "What is  $a + $b $name?"
+sleep 5
+echo $c
+echo "What is $a - $b $name?"
+sleep 5
+echo $d
+echo "What is $a \* $b $name?"
+sleep 5
+ echo $e
+echo "What is $a / $b $name?"
+sleep 5
+echo $f
+}
+sleep 5
+            echo "$name pick a number"
+read a
+                    echo "$name pick another number"
+read b
+        add  $a $b
+                echo "Thank you for playing.  Have a great Army day!"
+sleep 5
