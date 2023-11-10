f80c5f38c7f81498a0aaaec80dd6317b6d568343
Joseph Peters <joseph.peters75.com>
2023-11-10 14:33:50 -0500

    update

--- Unit 103/201ops4students.sh
+++ Unit 103/201ops4students.sh
@@ -4,22 +4,32 @@
 # Lets create a function in bash that adds two number together
 # Stretch goal can you do subtraction, multipliaction or division
 # You will need to declare two variables
+
+echo "What is your name?"
+
+read name
+
 add()
 {
 c=`expr $a + $b`
 d=`expr $a - $b`
 e=`expr $a \* $b`
 f=`expr $a / $b`
-echo "The Addition of two numbers is $c"
-echo "The two numbers subtracted is $d"
-echo "The two numbers divided is $e"
-echo "The two numbers multiplied is $f"
+echo "Addition is $a + $b = $c"
+sleep 4
+echo "Subtraction is $a - $b = $d"
+sleep 4
+echo "Multiplication is $a \* $b = $e"
+sleep 4
+echo "Division is $a / $b = $f"
+sleep 4
 }
 
-echo "Add the first number"
+echo "$name pick a number"
 read a
-echo "Add the second number"
+echo "$name pick another number"
 read b
 
 add  $a $b
+sleep 3
 echo "Have a great Army day!"
