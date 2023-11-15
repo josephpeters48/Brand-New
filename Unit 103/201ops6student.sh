5dac925dd3b5e4319381641baa214e78524a8f14
Joseph Peters <joseph.peters75.com>
2023-11-14 19:56:24 -0500

    update

--- Unit 103/201ops6student.sh
+++ Unit 103/201ops6student.sh
@@ -15,18 +15,12 @@ echo "what is your website"
 read website
 website=$website
 }
-
 website
 website
 website
 website
-
 dig $website	1>>single.txt
-
 whois $website 1>>single.txt
-
 host $website 1>>single.txt
-
 nslookup $website 1>>single.txt
-
 open single.txt
