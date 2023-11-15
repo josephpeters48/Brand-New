f030b18bafb77de3905dd90e155cd121ca7911dd
Joseph Peters <joseph.peters75.com>
2023-11-14 19:23:42 -0500

    update

--- Unit 103/201ops6student.sh
+++ Unit 103/201ops6student.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Script: Ops 201 Class 06 Ops Challenge Solution
+# Author: Joseph Peters
+# Take a user input string. Presumably the string is a domain name such as Google.com.
+# Run whois against a user input string.
+# Run dig against the user input string.
+# Run host against the user input string.
+# Run nslookup against the user input string.
+# Output the results to a single .txt file and open it with your favorite text editor.
+# For this challenge you must use at least one variable and one function.
+
 website(){
 echo "what is your website"
 read website
