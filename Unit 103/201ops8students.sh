6de4fe010402b1194579c87b87dc7825ed65090b
Joseph Peters <joseph.peters75.com>
2023-11-16 19:49:31 -0500

    update

--- Unit 103/201ops8students.sh
+++ Unit 103/201ops8students.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Script: Ops 301 Class 08 Ops Challenge Solution
+# Author: Joseph Peters
+# Purpose: DDOS
+#Lets create a script that would work like a DDOS attack by using a while loop
+#In this script we want to contiune to ping our personal pc in a infitine while loop
+#Somebody that had a control of a bot net could set up this script on thousands of computer and ping sites till they are overloaded and crash
 
 ping 192.168.10.128
 
