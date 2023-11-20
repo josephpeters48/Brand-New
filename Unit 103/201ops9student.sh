32941bab3874febc39ffd27a954a758bceb10396
Joseph Peters <146798137+josephpeters48@users.noreply.github.com>
2023-11-20 14:20:03 -0500

    Update 201ops9student.sh

--- Unit 103/201ops9student.sh
+++ Unit 103/201ops9student.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+
+
+# Lets create a while loop than runs a conditinal statment
+#Ask the user for an input if they choose:
+# 1 then echo hello world
+# 2 ping a website or ip address
+# 3 run ifconfig
+# else echo invalid entry
 echo "What is your favorite color"
 
 read color
