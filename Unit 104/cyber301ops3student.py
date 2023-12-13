0c37fdc9cf3180aac05787016f8c525e3f26267a
Joseph Peters <146798137+josephpeters48@users.noreply.github.com>
2023-12-13 15:29:03 -0500

    Add files via upload

---
+++ Unit 104/cyber301ops3student.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a Number:"))
+#if number < 100:
+ #   print("False.")
+#elif number >= 100:
+ #   print("True.")
+#print (number < 100  number >= 100)
+print (number >=  100)
+
+
