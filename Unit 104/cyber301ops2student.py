11d5482de606530ee3b22b4ebb0545194236288f
Joseph Peters <joseph.peters75.com>
2023-12-12 20:06:11 -0500

    update

---
+++ Unit 104/cyber301ops2student.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a Number:"))
+if number > 66:
+    print("Outstanding.")
+elif number == 65:
+    print("Do better.")
+else:
+    print("The number is neither equal or greater than 65.")
