0c661383afe819997e60f6e5810caf1493dece33
Joseph Peters <146798137+josephpeters48@users.noreply.github.com>
2023-12-15 15:50:37 -0500

    Add files via upload

---
+++ Unit 104/cyber301ops6students.py
@@ -0,0 +1,12 @@
+print  ("Test your luck")
+magic = int(input( "Pick a random number " ))
+import random
+random_number = random.randint(0, 10)
+while  magic != random_number:
+	print ("Ha Ha you are stuck in my loop")
+	magic = int(input( "Pick a random single digit number "))
+	magic == random_number
+else:	
+	print (f"The magic number is {random_number}. Well done mugle, you are free")
+
+	
