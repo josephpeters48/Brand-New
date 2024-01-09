b989deababb85a4749b16c825f05fb299838d73c
Joseph Peters <joseph.peters75.com>
2024-01-08 20:54:21 -0500

    update

--- 10.py
+++ 10.py
@@ -1,4 +1,9 @@
 import requests
-url="https://www.espn.com"
+url = input("What is your website: ")
+b = input ("Get, Post, Put, Delete, Head, Patch , Options: ")
 response = requests.get(url)
+response = requests.post(url)
+response = requests.put(url)
+response = requests.delete(url)
+response = requests.head(url)
 print(response.status_code)
