ca8698c9ef1215c295ad21d33c77d7684429c786
Joseph Peters <joseph.peters75.com>
2024-01-11 20:40:30 -0500

    update

--- 10.py
+++
@@ -1,9 +0,0 @@
-import requests
-url = input("What is your website: ")
-b = input ("Get, Post, Put, Delete, Head, Patch , Options: ")
-response = requests.get(url)
-response = requests.post(url)
-response = requests.put(url)
-response = requests.delete(url)
-response = requests.head(url)
-print(response.status_code)
