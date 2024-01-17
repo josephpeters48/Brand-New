a2e0a75757a05e31c42f49e1efac12e38113d54a
Joseph Peters <joseph.peters75.com>
2024-01-16 20:40:04 -0500

    update

--- cyber301ops12student.py
+++ cyber301ops12student.py
@@ -1,8 +1,9 @@
 # Your challenge tonight is to write a basic adventure game using some of the  
 #code provide below and using if/elif statments
+import random
 
 yes_no = ["yes", "no"]
-directions = ["north", "south", "east", "west"]
+directions = ["left, right"]
  
 # Introduction
 name = input("What is your name, adventurer?\n")
@@ -28,9 +29,9 @@ response = ""
 # Use if else statment from here to take you on a journey and have fun with it
 while True:
     print("Which direction would you like to go?:")
-    response = input("What direction do you move?\nleft/right/forward/backward\n")
+    response = input("What direction do you move?\nleft/right ")
     
-    if response == "north":
+    if response == "left":
         print("You ran into a chicken in a condo made of peanuts what do you do?")
         fight = input("fight the chicken y/n\n")
         if  fight == "y":
@@ -42,3 +43,10 @@ while True:
             else: print("Your roll did not achieve a 4 or better") 
             quit()
         else: print("run for your life")
+    elif response == "right":
+        print("You just keep going and going.  You find a hotel to the left of you.\n")
+        hotel = input("You enter the hotel do you go left or right?\n")
+        if hotel == "left":
+            print("You go to sleep.")
+            quit()
+                
