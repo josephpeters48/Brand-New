fc856e108aa9c6b2f21541e6d4ad41a25db59459
Joseph Peters <joseph.peters75.com>
2024-01-16 19:52:40 -0500

    update

--- password.py
+++ password.py
@@ -1,3 +1,14 @@
 import string
 import random
-len=int(input("Enter Length of Password"))
+len=int(input("Enter Length of password "))
+#lower=string.ascii_lowercase
+#upper=string.ascii_uppercase
+#digit=string.digits
+#symbols=string.punctuation
+
+#str=lower+upper+digit+symbols
+str=string.printable
+
+pwd=random.sample(str,len)
+password="".join(pwd)
+print(password)
