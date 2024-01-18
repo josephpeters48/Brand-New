cccd12d57065060e367298b09c1c13fb3b0012f3
Joseph Peters <joseph.peters75.com>
2024-01-17 22:01:49 -0500

    update

--- cyber301ops12student.py
+++ cyber301ops12student.py
@@ -1,36 +1,36 @@
 # Your challenge tonight is to write a basic adventure game using some of the  
 #code provide below and using if/elif statments
 import random
-
-yes_no = ["yes", "no"]
+import time
+#yes_no = ["yes", "no"]
 directions = ["left, right"]
- 
+menu = ("yes", "no")
 # Introduction
-name = input("What is your name, adventurer?\n")
-print("Greetings, " + name + ". Let us go on a quest!")
-print("You find yourself on the edge of a dark forest.")
-print("Can you find your way through?\n")
+name = input("Name your self Smart one?\n")
+print("Howdy, " + name + ". Shall we play a game!")
+print("As sky becomes light you look all around to find yourself in a land of snow.")
+print("With no tracks and no since of direction can you "+ name +" find your way to safety?\n")
  
 # Start of game
 response = ""
-while response not in yes_no:
-    response = input("Would you like to step into the forest?\nyes/no\n")
-    if response == "yes":
-        print("You head into the forest. You hear crows cawwing in the distance.\n")
-    elif response == "no":
-        print("You are not ready for this quest. Goodbye, " + name + ".")
+while response != 1 not in menu:
+    print()
+    print("** Menu **")
+    print("1 = yes")
+    print("2 = no")   
+    response = input("Do you want to put Snow Shoes on? ")
+    if response == "1":
+        print("Good Choice. As you put your Snow Shoes on you start walking.\n")
+    elif response == "2":
+        print("You find out the snow is to deep and soon you will die.  You might be missed!!!!! " + name + ".")
         quit()
     else: 
         print("I didn't understand that.\n")
- 
-# Next part of game
-response = ""
 
-# Use if else statment from here to take you on a journey and have fun with it
-while True:
+response = ""
+while response not in directions:
     print("Which direction would you like to go?:")
     response = input("What direction do you move?\nleft/right ")
-    
     if response == "left":
         print("You ran into a chicken in a condo made of peanuts what do you do?")
         fight = input("fight the chicken y/n\n")
